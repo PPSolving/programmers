824b5b70bede537a2f48516894632116c776c118
kbm6609 <kbm6609@naver.com>
2021-05-24 14:24:51 +0900

    k번째수 21/05/24

---
+++ kbm6609/Level1/K번째수.java
@@ -0,0 +1,18 @@
+package Kth_number;
+
+import java.util.Arrays;
+
+public class Kthnumber_210524001 {
+    public static void main(String[] args) {
+        int[] array={1, 5, 2, 6, 3, 7, 4};
+        int[][] commands={{2, 5, 3},{4, 4, 1},{1, 7, 3}};
+        int answer[] = new int[commands.length];
+        for(int i=0;i< commands.length;i++){
+            int[] tmp =Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(tmp);
+            answer[i] =tmp[commands[i][2]-1];
+        }
+
+        for(int i:answer) System.out.println(i);
+    }
+}
