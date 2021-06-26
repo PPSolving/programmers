89a180e90ea2b18dbb98fa70d7115edbc87e119e
kbm6609 <kbm6609@naver.com>
2021-06-26 23:42:57 +0900

    단체사진 찍기 누락된거 추가

--- kbm6609/Level1/단체사진 찍기_210625001.java
+++ kbm6609/Level1/단체사진 찍기_210625001.java
@@ -1,42 +1,49 @@
 package Group_Photo;
 
+
 import java.util.HashMap;
-import java.util.Stack;
 
-class condition{
-    HashMap<Character,Integer> use =new HashMap<>();// 조건에 속하는 사람을 표시해주는 해쉬맵
-    HashMap<Integer,Integer> condi =new HashMap<>();// 조건을 최종 2진수로 만든 해쉬맵
-
-    HashMap<Integer,Character> a =new HashMap<>(); //이 두개는 배열대신 사용되는 해쉬맵
-    HashMap<Character,Integer> b =new HashMap<>();
-
-
-
-    Stack<String> con =new Stack<>();
-    String t =new String();
-    void inputcon(String str){
-        use.put(str.charAt(0),1);
-        use.put(str.charAt(2),1);
-        con.push(str);
-    }
-    void creatcon(){
-        while(!con.empty())
-        t = con.pop();
-        if(t.charAt(3)=='='){
-
-        }
-        else if(t.charAt(3)=='<'){
-
-        }
-        else if(t.charAt(3)=='>'){
-
-        }
-    }
-
-}
 public class GroupPhoto_210625001 {
     public static void main(String[] args) {
-
+        String[] data={"N~F=0", "R~T>2"};
+        int answer =0;
+        char[][] ch ={{'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'},{'0','0','0','0','0','0','0','0'}};
+        HashMap<Character,Integer> hs=new HashMap<>();
+        answer =run(hs,ch,0,data);
+        System.out.println(answer);
     }
-
+    static int run(HashMap<Character,Integer> h, char[][] ch, int index, String[] data){
+        HashMap<Character,Integer> hs =new HashMap<>(h);
+        int sum=0;
+        if(index ==ch[0].length){
+            sum +=check(hs,data);
+        }
+        else{
+            for(int j= 0;j<ch[0].length;j++){
+                if(ch[1][j]=='0'){
+                    hs.put(ch[0][j],index);
+                    ch[1][j] ='1';
+                    sum+=run(hs,ch,index+1,data);
+                    ch[1][j]='0';
+                    hs.remove(ch[0][j]);
+                }
+            }
+        }
+        return sum;
+    }
+    static int check(HashMap<Character, Integer> hs,String[] data){
+        int re=0;
+        for (int i = 0; i < data.length; i++) {
+            int a=Math.abs(hs.get(data[i].charAt(0))-hs.get(data[i].charAt(2)))-1;
+            int b=(int)(data[i].charAt(4)-'0');
+            if (data[i].charAt(3) == '=' && a ==b) re=1;
+            else if(data[i].charAt(3) == '>'&&a >b) re=1;
+            else if(data[i].charAt(3) == '<'&&a < b)re=1;
+            else{
+                re =0;
+                break;
+            }
+        }
+        return re;
+    }
 }
