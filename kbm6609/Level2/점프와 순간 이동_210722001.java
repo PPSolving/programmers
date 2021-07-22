87fe78e96831ed02e93eadf795ba5b9345544131
kbm6609 <kbm6609@naver.com>
2021-07-23 02:22:52 +0900

    210722

---
+++ kbm6609/Level2/점프와 순간 이동_210722001.java
@@ -0,0 +1,15 @@
+package Jump_And_Teleportation;
+
+import java.util.ArrayList;
+
+public class JumpAndTeleportation_210722001 {
+    public static void main(String[] args) {
+        int n =5000;
+        int answer=0;
+        char[] ch =Integer.toBinaryString(1000000000).toCharArray();
+        for (char i : ch) if(i=='1') answer++;
+
+
+    }
+
+}
